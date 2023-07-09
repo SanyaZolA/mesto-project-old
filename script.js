a761acb106da2bb11f9f854f14818973e684cfce
Alexandr Sergeev <ntl1221@yandex.ru>
2023-07-09 23:33:58 +0300

    upd fix

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-const popup = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup__profile');
+const popupAddFoto = document.querySelector('.popup__addfoto');
 const formElement = document.querySelector('.popup__input-form');
 const inputName = document.querySelector('.popup__input_type_name');
 const inputAbout = document.querySelector('.popup__input_type_about');
 const profileEditButtonOpen = document.querySelector('.profile__edit-button');
-const profileEditButtonClose = document.querySelector('.popup__close-button');
+const closeProfileButton = document.querySelector('section.popup__profile .popup__close-button');
+const closeFotoButton = document.querySelector('section.popup__addfoto .popup__close-button');
 const profileEditButtonSave = document.querySelector('.popup__save-button');
 const profileName = document.querySelector('.profile__name');
 const profileWho = document.querySelector('.profile__who')
+const fotoAdd = document.querySelector('.profile__add-button');
 
-//----------------------ОТКРЫТИЕ ПОПАПА------------------------------
+//----------------------ОТКРЫТИЕ ПОПАПА (ПРОФИЛЬ)------------------------------
 profileEditButtonOpen.addEventListener('click', openPopup);
 
 function openPopup() {
-  popup.classList.add('popup_opened');
+  popupProfile.classList.add('popup_opened');
   inputName.value = '';
   inputAbout.value = '';
 }
-//----------------------ЗАКРЫТИЕ ПОПАПА ЧЕРЕЗ КРЕСТ------------------------------
-profileEditButtonClose.addEventListener('click', closePopup);
+//----------------------ЗАКРЫТИЕ ПОПАПА ЧЕРЕЗ КРЕСТ (ПРОФИЛЬ)------------------------------
+closeProfileButton.addEventListener('click', closePopupProfile);
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function closePopupProfile() {
+  popupProfile.classList.remove('popup_opened');
+  return closePopup1;
 }
-//----------------------ЗАКРЫТИЕ ПОПАПА ЧЕРЕЗ СОХРАНИТЬ------------------------------
+//----------------------ЗАКРЫТИЕ ПОПАПА ЧЕРЕЗ СОХРАНИТЬ И СОХРАНЕНИЕ ИНФОРМАЦИИ------------------------------
 profileEditButtonSave.addEventListener('click', savePopup);
 
 function savePopup(evt) {
   evt.preventDefault()
-  popup.classList.remove('popup_opened');
-  if (inputName.value.length != 0 && inputAbout.value.length != 0 ) { 
+  popupProfile.classList.remove('popup_opened');
+  if (inputName.value.length != 0 && inputAbout.value.length == 0) { 
+    profileName.textContent = `${inputName.value}`
+  } else if (inputName.value.length == 0 && inputAbout.value.length != 0) { 
+    profileWho.textContent = `${inputAbout.value}`
+  } else if (inputName.value.length != 0 && inputAbout.value.length != 0) { 
     profileName.textContent = `${inputName.value}`
     profileWho.textContent = `${inputAbout.value}`
-    return savePopup;
-  }
 }
+return savePopup;
+}
+
+//----------------------ОТКРЫТИЕ ПОПАПА (добавление картинок)------------------------------
+fotoAdd.addEventListener('click', openAddFoto);
+
+function openAddFoto() {
+  popupAddFoto.classList.add('popup_opened');
+  inputName.value = '';
+  inputAbout.value = '';
+}
+
+//----------------------ЗАКРЫТИЕ ПОПАПА ЧЕРЕЗ КРЕСТ (добавление картинок)------------------------------
+  closeFotoButton.addEventListener('click', closePopupAddFoto);
+
+  function closePopupAddFoto() {
+    popupAddFoto.classList.remove('popup_opened');
+    return closePopup;
+  }
