3573739f8b50fd2e4649d0cbbe34d0312552adfb
Alexandr Sergeev <ntl1221@yandex.ru>
2023-07-13 16:09:57 +0300

    JS upd

--- script/script.js
+++ script/script.js
@@ -26,12 +26,12 @@ const formEditProfile = popupProfile.querySelector('.popup__input-form');  // ф
 const formAddFoto = popupAddFoto.querySelector('.popup__input-form');  // форма ввода добавление картинок.
 
 // //----------------------------------НАХОЖДЕНИЕ КАРТИНОК/ТЕКСТА ДЛЯ ЗУМА----------------------------------------------------------------
-const imageZoom = popupZoom.querySelector('.popup__zoom-image');
-const signatureZoom = popupZoom.querySelector('.popup__zoom-signature');
+const imageZoom = popupZoom.querySelector('.popup__zoom-image');  // для зума ищем элемент картинки
+const signatureZoom = popupZoom.querySelector('.popup__zoom-signature'); // для зума ищем элемент подписи
 
 // //----------------------------------НАХОЖДЕНИЕ МЕСТА РАСПОЛОЖЕНИЕ ЛИСТА В HTML И ЭЛЕМЕНТ КАРТОЧКИ-------------------------------
-const elementList = document.querySelector('.element__list');
-const elementTemplate = document.querySelector('.element__template').content;
+const elementList = document.querySelector('.element__list'); // вешаем элемент списка на переменную.
+const elementTemplate = document.querySelector('.element__template').content; // переменная получает данные из блока HTML template
 
 // //----------------------------------ФУНКЦИИ ДЛЯ ОТКРЫТИЯ И ЗАКРЫТИЯ ПОПАПОВ-----------------------------------------------------
 function openPopupProfile() {
@@ -62,19 +62,19 @@ function closePopup(popup) {
   popup.classList.remove('popup_opened')};                        // функция для открытия(многоразовая).
 
 function openZoomPopup(evt) {                                // функция для открытия зума
-  imageZoom.src = evt.target.src;                            // берет ссылку из определенного поста
-  imageZoom.alt = evt.target.alt;                             // берет ссылку из определенного поста
-  signatureZoom.textContent = evt.target.alt;                 // берет текст для зума
-  openPopup(popupZoom);                                       // и собственно открывает.
+  imageZoom.src = evt.target.src;                            // берет ссылку из определенного поста (кликнутого);
+  imageZoom.alt = evt.target.alt;                             // берет ссылку из определенного поста (кликнутого);
+  signatureZoom.textContent = evt.target.alt;                 // берет текст для зума из определенного поста (кликнутого);
+  openPopup(popupZoom);                                       // и собственно открываем.
 };
 
-function formAddNewFoto(evt) {                                              // заполненная форма встаёт в список.
-  evt.preventDefault();                                                     // сброс отправки
-  const newElement = createNewPost(inputNameFoto.value, inputLinkFoto.value);   // берёт то что заполнили
-  elementList.prepend(newElement);                                              // новая фотка идёт в начало.
+function formAddNewFoto(evt) {                                                  // заполненная форма встаёт в список.
+  evt.preventDefault();                                                          // сброс стандартной отправки
+  const newElement = createNewPost(inputNameFoto.value, inputLinkFoto.value);  // берёт то что заполнили в форме в добавляем в переменную
+  elementList.prepend(newElement);                                              // новый пост идёт в начало листа.
   closePopup(popupAddFoto)                                                      // делает функцию для закрытия.
 };
-formAddFoto.addEventListener('submit', formAddNewFoto);
+formAddFoto.addEventListener('submit', formAddNewFoto);                         // вешаем событие(закрыть) на событие 'sudmit' - отправка
 
 // //----------------------------------ОТКРЫТИЕ/ЗАКРЫТИЕ ПОПАПОВ ПРОФИЛЯ И КАРТИНОК------------------------------------------------
 buttonEditPopupProfile.addEventListener('click', openPopupProfile );    // вешаем событие(открыть) редактирование профиля на клик мыши.
@@ -88,11 +88,11 @@ buttonClosePopupZoom.addEventListener('click', closeZoomFoto);         // веш
 // //----------------------------------СОХРАНЕНИЕ ИНФОРМАЦИИ ПРИ РЕДАКТИРОВАНИИ ПРОФИЛЯ--------------------------------------------
 function saveProfilePopup (e) {
   e.preventDefault();                                          // отмена стандартной формы отправки.
-  profileName.textContent = inputNameProfile.value;              // сохранение введенной информации ФИО.
-  profileWho.textContent = inputAboutProfile.value;              // сохранение введенной информации О СЕБЕ.
+  profileName.textContent = inputNameProfile.value;              // сохранение введенной информации ФИО и замена в профиле.
+  profileWho.textContent = inputAboutProfile.value;              // сохранение введенной информации О СЕБЕ и замена в профиле.
   closePopup(popupProfile);      
 };
-formEditProfile.addEventListener('submit', saveProfilePopup);  // вешаем событие(закрыть) на событие 'sudmit' - отправка.
+formEditProfile.addEventListener('submit', saveProfilePopup);  // на форму вешаем событие(сохранить данные) при 'оптравить'.
 
 // //----------------------------------МАССИВ КАРТИНОВ------------------------------------------------------------------------------
 const initialCards = [                                         // добавляем в переменную массив картинок и названий.
@@ -110,23 +110,23 @@ const initialCards = [                                         // добавля
   link: 'https://images.unsplash.com/photo-1606730004798-e77a54f5e46e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'}
   ];
 
-// //----------------------------------ФУНКЦИЯ СОЗДАНИЯ НОВОГО ПОСТА-------------------------------------------------------------------
+// //----------------------------------ФУНКЦИИ СОЗДАНИЯ СПИСКА КАРТИНОК-------------------------------------------------------------------
 function createNewPost(postName, imageLink) {
-  const onePost = elementTemplate.cloneNode(true);                     // копируем в переменную данные.
-  const postImage = onePost.querySelector('.element__foto');           // даём переменной.
-  postImage.src = imageLink;                                           // переменная получает новые данные из попапа ввода.
-  onePost.querySelector('.element__title').textContent = postName;      // переменная получает новые данные из попапа ввода.
-  postImage.alt = postName;                                              // переменная получает новые данные из попапа ввода.
-  onePost.querySelector('.element__like-button').addEventListener('click', likeButton);         // вешаем кнопку лайка
-  onePost.querySelector('.element__trash-button').addEventListener('click', closeNewElement);  // вышаем кнопку удаления
-  postImage.addEventListener('click', openZoomPopup);                     // вешаем возможность делать зум-зум
+  const onePost = elementTemplate.cloneNode(true);                     // копируем в переменную данные из template в HTML.
+  const postImage = onePost.querySelector('.element__foto');           // ищем данные класс в template и вешаем его на переменную.
+  postImage.src = imageLink;                                           // Элемент фото template получает данные из параментов функции.
+  postImage.alt = postName;                                             // Элемент фото template получает данные из параментов функции.
+  onePost.querySelector('.element__title').textContent = postName;                         // ищем класс и добавляем туда новый параметр                   
+  onePost.querySelector('.element__like-button').addEventListener('click', likeButton);         // ищем класс и вешаем кнопку лайка
+  onePost.querySelector('.element__trash-button').addEventListener('click', closeNewElement);   // ищем класс и вышаем кнопку удаления
+  postImage.addEventListener('click', openZoomPopup);                     // вешаем возможность делать зум-зум.
   return onePost;
 };
 
-function initializeList(list) {                                           // создаётся список видимый.
-  list.forEach(function (item) {                                            // для каждого элемента
-    const onePost = createNewPost(item.name, item.link);                    // берётся название и ссылка.
-    elementList.append(onePost);                                            // выстраивается список на странице
+function initializeList(list) {                                     // создаётся список картинок для отображения на странице.
+  list.forEach(function (item) {                                    // в списке для каждого элемента выполняется функция.
+    const onePost = createNewPost(item.name, item.link);            // получает название и ссылку. Формируется пост. 6 раз
+    elementList.append(onePost);                                    // каждая картинки ставится в начало листа template.
   });
 };
-initializeList(initialCards);                                                 // сама инициализация.
+initializeList(initialCards);    // сама инициализация предустановленных картинок. Начинается формироваться список при загрузке страницы                                                                  //Начинается
